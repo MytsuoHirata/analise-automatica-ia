0ee12a9103b7229327826deb89e0135ba432e656
Mytsuo Hirata <Mytsuohirata00@gmail.com>
2025-12-23 21:27:32 -0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,27 @@
 "use client";
 
-import MatrixRain from "../components/MatrixRain";
 import { useState } from "react";
-import Dashboard from "./dashboard/page";
+import MatrixRain from "../components/MatrixRain";
+import Dashboard from "../app/dashboard/page"; 
 
 export default function Home() {
   const [enter, setEnter] = useState(false);
 
+  // Quando clicar no botão, troca pra dashboard
   if (enter) return <Dashboard />;
 
   return (
     <div className="relative w-full h-screen bg-black text-green-400 font-mono flex items-center justify-center">
+
       {/* Chuva de código */}
       <div className="absolute inset-0 -z-10">
         <MatrixRain />
       </div>
 
+      {/* Tela inicial */}
       <div className="text-center space-y-6">
-        <h1 className="text-4xl tracking-widest">MATRIX ACCESS PANEL</h1>
+        <h1 className="text-4xl tracking-widest">S Y N T R A X</h1>
+
         <button
           onClick={() => setEnter(true)}
           className="border border-green-500 px-6 py-3 hover:bg-green-500 hover:text-black transition-all"
@@ -25,6 +29,7 @@ export default function Home() {
           ENTER SYSTEM
         </button>
       </div>
+
     </div>
   );
 }
